585f2e4d8140c95aeedfe6c13513a6cd79a8dfcb
SohamAgrawal <109532976+SohamAgrawal@users.noreply.github.com>
2023-01-11 15:19:01 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CHINMAY";
+const accessCode2 = "SARAOGI";
+const accessCode3 = "SPECTACLES";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("H M I C N Y A", 100,50)
+    fill("lightblue")
+    text("Hint: My best friend !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("S O R A G A I", 700,150)
+    fill("lightblue")
+    text("Hint: My friend's surname !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("P L C T S E C A E S ", 100,250)
+    fill("lightblue")
+    text("Hint: He wears it !!", 100,270)
+
+}
